3403031db3adadd1fde29f2c72c3f9015db1f780
Antonio <itonygame@gmail.com>
2019-08-19 14:54:42 -0700

    Pantalla de colores

---
+++ Practica 6 clases/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Practica 6 clases
+//
+//  Created by equipo on 8/19/19.
+//  Copyright © 2019 equipo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Practica 6 clasesTests/Practica_6_clasesTests.swift
@@ -0,0 +1,34 @@
+//
+//  Practica_6_clasesTests.swift
+//  Practica 6 clasesTests
+//
+//  Created by equipo on 8/19/19.
+//  Copyright © 2019 equipo. All rights reserved.
+//
+
+import XCTest
+@testable import Practica_6_clases
+
+class Practica_6_clasesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
